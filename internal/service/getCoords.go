415c760af60a00b5c9729bf2c95e25b8396e5e23
tursynkhan <tursynkhan001@gmail.com>
2022-10-30 20:37:51 +0600

    Initial commit

---
+++ internal/service/getCoords.go
@@ -0,0 +1,6 @@
+package service
+
+// func GetCoordOfCity(datesLocations map[string][]string) [][]string {
+// 	coords := [][]string{}
+// 	url := "https://geocode-maps.yandex.ru/1.x?apikey=a8ff2dc8-4e35-4012-85c3-ddcba1569483&format=json&results=2&geocode="+city
+// }
